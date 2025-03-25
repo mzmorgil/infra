0619b3b1da49d430dadae32645341cebf6a8cc38
Maxim Veksler <maxim@vekslers.org>
2025-03-25 12:13:29 +0200

    Working cloudflared tunnel

--- cloudflare.tf
+++ cloudflare.tf
@@ -6,46 +6,70 @@ data "cloudflare_zones" "self" {
   name = var.cloudflare_zone
 }
 
-### cloudflare
+### Cloudflare Resources
 
-resource "cloudflare_zero_trust_tunnel_cloudflared" "self" {
-  account_id = var.cloudflare_account_id
-  name       = "mzm-gke-cluster"
-  config_src = "cloudflare" # forces replacement https://github.com/cloudflare/terraform-provider-cloudflare/issues/5363
-}
+# Due to existing bug that causes replacement we will use tunnel_id from variable.
+# Resource was created by IaC, than removed from state. 
+# Once bug is fixed, will be resumed to state by an import
 
-# Creates the CNAME record that routes http_app.${var.cloudflare_zone} to the tunnel.
+# https://github.com/cloudflare/terraform-provider-cloudflare/issues/5363
+# resource "cloudflare_zero_trust_tunnel_cloudflared" "self" {
+#   account_id = var.cloudflare_account_id
+#   name       = "mzm-gke-cluster"
+#   config_src = "cloudflare" # forces replacement 
+# }
+
 resource "cloudflare_dns_record" "mzm-gke-cluster" {
   zone_id = local.cloudflare_zone_id
   name    = var.cloudflare_zone
-  content = "${cloudflare_zero_trust_tunnel_cloudflared.self.id}.cfargotunnel.com"
+  content = "${var.cloudflare_zero_trust_tunnel_cloudflared_id}.cfargotunnel.com"
   type    = "CNAME"
   proxied = true
   ttl     = "1"
 }
 
-# Creates the configuration for the tunnel.
-resource "cloudflare_zero_trust_tunnel_cloudflared_config" "auto_tunnel" {
+resource "cloudflare_zero_trust_tunnel_cloudflared_config" "self" {
   account_id = var.cloudflare_account_id
-  tunnel_id  = cloudflare_zero_trust_tunnel_cloudflared.self.id
+  tunnel_id  = var.cloudflare_zero_trust_tunnel_cloudflared_id
+  source     = "cloudflare"
+
   config = {
     ingress = [{
       hostname = "${cloudflare_dns_record.mzm-gke-cluster.name}"
-      service  = "http://httpbin:8080"
+      service  = "http://ingress-nginx-controller.nginx-ingress.svc.cluster.local:80"
       }, {
       service = "http_status:404"
     }]
   }
 }
 
-### k8s cloudflared
-
+### Kubernetes Resources
 resource "kubernetes_namespace" "cloudflared" {
   metadata {
     name = "cloudflared"
   }
 }
 
+data "cloudflare_zero_trust_tunnel_cloudflared_token" "self" {
+  account_id = var.cloudflare_account_id
+  tunnel_id  = var.cloudflare_zero_trust_tunnel_cloudflared_id
+}
+
+# Create a Kubernetes Secret to store the tunnel token
+resource "kubernetes_secret" "cloudflared_tunnel_token" {
+  metadata {
+    name      = "cloudflared-tunnel-token"
+    namespace = kubernetes_namespace.cloudflared.metadata[0].name
+  }
+
+  data = {
+    "tunnel-token" = data.cloudflare_zero_trust_tunnel_cloudflared_token.self.token
+  }
+
+  type = "Opaque"
+}
+
+# Updated Kubernetes Deployment to use the Secret
 resource "kubernetes_deployment" "cloudflared" {
   metadata {
     name      = "cloudflared"
@@ -72,18 +96,27 @@ resource "kubernetes_deployment" "cloudflared" {
         container {
           image = "cloudflare/cloudflared:latest"
           name  = "cloudflared"
-          args  = ["tunnel", "run", "--token", "<<tunnel_token>>"]
+          args  = ["tunnel", "run", "--token", "$(TUNNEL_TOKEN)"] # Reference the env var
 
-          liveness_probe {
-            http_get {
-              path = "/ready"
-              port = 2000
+          env {
+            name = "TUNNEL_TOKEN"
+            value_from {
+              secret_key_ref {
+                name = kubernetes_secret.cloudflared_tunnel_token.metadata[0].name
+                key  = "tunnel-token"
+              }
             }
-
-            initial_delay_seconds = 1
-            period_seconds        = 10
-            failure_threshold     = 1
           }
+
+          # liveness_probe {
+          #   http_get {
+          #     path = "/ready"
+          #     port = 2000
+          #   }
+          #   initial_delay_seconds = 1
+          #   period_seconds        = 10
+          #   failure_threshold     = 1
+          # }
         }
       }
     }
--- variables.tf
+++ variables.tf
@@ -41,3 +41,9 @@ variable "cloudflare_api_token" {
   type        = string
   sensitive   = true
 }
+
+variable "cloudflare_zero_trust_tunnel_cloudflared_id" {
+  description = "Cloudflare Zero Trust Tunnel Cloudflared ID"
+  type        = string
+  sensitive   = true
+}
