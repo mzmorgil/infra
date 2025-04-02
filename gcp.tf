8a096ee3727d8dddee345eabee3c14680ee37bbb
Maxim Veksler <maxim@vekslers.org>
2025-04-02 22:54:19 +0300

    Sync

--- gcp.tf
+++ gcp.tf
@@ -1,86 +1,3 @@
-# Enable required APIs using for_each
-resource "google_project_service" "apis" {
-  for_each = toset([
-    "cloudresourcemanager.googleapis.com",
-    "compute.googleapis.com",
-    "container.googleapis.com",
-    "storage.googleapis.com",
-    "iam.googleapis.com",
-  ])
-  service            = each.key
-  project            = var.project_id
-  disable_on_destroy = false
-}
-
-# GCS Bucket for Terraform State
-resource "google_storage_bucket" "terraform_state" {
-  name                        = "mzm-org-il-terraform-state"
-  location                    = "ME-WEST1"
-  force_destroy               = false
-  uniform_bucket_level_access = true
-
-  versioning {
-    enabled = true
-  }
-
-  lifecycle_rule {
-    action {
-      type = "Delete"
-    }
-    condition {
-      num_newer_versions = 5
-    }
-  }
-}
-
-# Workload Identity Pool for GitHub Actions
-resource "google_iam_workload_identity_pool" "github_pool" {
-  workload_identity_pool_id = "github-actions-pool"
-  display_name              = "GitHub Actions Pool"
-  description               = "Identity pool for GitHub Actions"
-  project                   = var.project_id
-}
-
-# Workload Identity Provider for GitHub Actions
-resource "google_iam_workload_identity_pool_provider" "github_provider" {
-  workload_identity_pool_id          = google_iam_workload_identity_pool.github_pool.workload_identity_pool_id
-  workload_identity_pool_provider_id = "github-provider"
-  display_name                       = "GitHub Provider"
-  description                        = "OIDC provider for GitHub Actions"
-  attribute_condition                = "assertion.repository_owner == 'mzmorgil'"
-  attribute_mapping = {
-    "google.subject"       = "assertion.sub"
-    "attribute.actor"      = "assertion.actor"
-    "attribute.repository" = "assertion.repository"
-  }
-  oidc {
-    issuer_uri = "https://token.actions.githubusercontent.com"
-  }
-}
-
-# Service Account for CI/CD (Terraform/GitHub Actions)
-resource "google_service_account" "cicd_sa" {
-  account_id   = "mzm-cicd-sa"
-  display_name = "CI/CD Service Account"
-  project      = var.project_id
-}
-
-# Grant CI/CD service account full admin access
-resource "google_project_iam_member" "cicd_sa_roles" {
-  for_each = toset([
-    "roles/owner"
-  ])
-  role    = each.key
-  member  = "serviceAccount:${google_service_account.cicd_sa.email}"
-  project = var.project_id
-}
-
-# Allow GitHub Actions to impersonate the CI/CD service account
-resource "google_service_account_iam_member" "github_impersonation" {
-  service_account_id = google_service_account.cicd_sa.name
-  role               = "roles/iam.workloadIdentityUser"
-  member             = "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.github_pool.name}/attribute.repository/mzmorgil/infra"
-}
 
 # Custom Service Account for GKE Nodes
 resource "google_service_account" "gke_node_sa" {
@@ -167,10 +84,6 @@ resource "google_container_cluster" "gke_cluster" {
   workload_identity_config {
     workload_pool = "${var.project_id}.svc.id.goog"
   }
-
-  depends_on = [
-    google_project_service.apis,
-  ]
 }
 
 # Spot Node Pool
