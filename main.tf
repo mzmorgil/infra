a72d2dbc4b2ea79ecdcef68ef87e60ae6ae9ce91
Maxim Veksler <maxim@vekslers.org>
2025-04-05 14:20:58 +0300

    Enable cloudflared

--- main.tf
+++ main.tf
@@ -1,7 +1,6 @@
-# module "mzm-gke-cloudflare" {
-#   count = 0
-#   source = "./tofu-modules/cloudflare"
-#   cloudflare_zone = var.cloudflare_zone
-#   cloudflare_account_id = var.cloudflare_account_id
-# }
-
+module "mzm-gke-cloudflare" {
+  count = 0
+  source = "./tofu-modules/cloudflare"
+  cloudflare_zone = var.cloudflare_zone
+  cloudflare_account_id = var.cloudflare_account_id
+}
